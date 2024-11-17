f2990fab76879e1c95518238909a789a756c5272
X_V.neer <xv.neer@gmail.com>
2024-11-17 18:32:23 +0200

    fix: fix build error

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,23 +1,17 @@
 "use client"
 
-import React from "react"
-import { useSearchParams } from "next/navigation"
 import { Card, CardBody } from "@nextui-org/card"
-
-import SendOTP from "./components/send-otp"
-import Success from "./components/success"
-import VerifyOTP from "./components/verify-opt"
+import { Suspense } from "react"
+import Wrapper from "./components/wrapper"
 
 const Page = () => {
-  const searchParams = useSearchParams()
-  const mobile = searchParams.get("mobile")
-  const success = searchParams.get("success")
-
   return (
     <section className="flex h-full items-center justify-center gap-4 ~/md:~py-8/10">
       <Card className="w-full max-w-sm border-none bg-[#0A090959] backdrop-blur-md">
         <CardBody className="p-8 rtl:text-right">
-          {success ? <Success /> : mobile ? <VerifyOTP /> : <SendOTP />}
+          <Suspense>
+            <Wrapper />
+          </Suspense>
         </CardBody>
       </Card>
     </section>
