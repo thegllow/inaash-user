c19e6e7bc98c30a8862266fe309efcbe05d6559e
X_V.neer <xv.neer@gmail.com>
2024-11-18 12:33:57 +0200

    feat: fix resposive for faqs and information center

--- src/app/[locale]/faqs/components/list.tsx
+++ src/app/[locale]/faqs/components/list.tsx
@@ -19,7 +19,7 @@ const List = (props: Props) => {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
   return (
-    <div className="~px-6/16">
+    <div className="~px-5/16">
       <Accordion
         itemClasses={{
           indicator: "group",
--- src/app/[locale]/information-center/page.tsx
+++ src/app/[locale]/information-center/page.tsx
@@ -5,15 +5,15 @@ import SearchResults from "./components/search-results"
 import { Divider } from "@nextui-org/divider"
 
 export default async function Page() {
-  const t = await getTranslations("faqs")
+  const t = await getTranslations("information-center")
   return (
     <section className="flex items-center justify-center gap-4 ~/md:~py-16/24">
       <Card className="w-full max-w-3xl shrink-0 rounded-xl bg-[#0A090959] p-1">
-        <div className="mb-3 rounded-t-lg bg-[#1D1B1B] ~px-9/12 ~py-6/9">
-          <h1 className="text-2xl">{t("title")}</h1>
-          <p className="text-sm text-default-500">{t("description")}</p>
+        <div className="mb-3 rounded-t-lg bg-[#1D1B1B] ~px-7/12 ~py-5/9">
+          <h1 className="~text-xl/2xl">{t("title")}</h1>
+          <p className="text-default-500 ~text-xs/sm">{t("description")}</p>
         </div>
-        <div className="space-y-8 ~px-10/16 ~py-5/7">
+        <div className="~space-y-4/8 ~px-6/16 ~py-5/7">
           <Search />
           <Divider />
           <SearchResults />
