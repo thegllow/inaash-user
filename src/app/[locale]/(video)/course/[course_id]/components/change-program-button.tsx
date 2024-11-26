065e7216be9a77a1ea77f8462f0d2474f9053984
X_V.neer <xv.neer@gmail.com>
2024-11-26 13:41:19 +0200

    feat: create change program dialog

--- src/app/[locale]/(video)/course/[course_id]/components/change-program-button.tsx
+++ src/app/[locale]/(video)/course/[course_id]/components/change-program-button.tsx
@@ -1,18 +1,25 @@
+import { cn } from "@/lib/cn"
 import { Link } from "@/lib/i18n/navigation"
-import { Button } from "@nextui-org/button"
 import { Card } from "@nextui-org/card"
 import { Image } from "@nextui-org/image"
 import { Modal, ModalBody, ModalContent, useDisclosure } from "@nextui-org/modal"
-import { ChevronDown, ChevronUp } from "lucide-react"
+import { ChevronUp } from "lucide-react"
 import { useVideos } from "../context/courses-context"
-import { cn } from "@/lib/cn"
+import { useState } from "react"
+import { useTranslations } from "next-intl"
+import MyButton from "@/components/ui/button"
+import { Button } from "@nextui-org/button"
+import { useParams } from "next/navigation"
 
 type Props = {
   children: String
 }
 
 const ChangeProgramButton = (props: Props) => {
+  const { course_id } = useParams()
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const [course, setCourse] = useState("")
+  const t = useTranslations("course.course-footer")
   const { videos } = useVideos()
   return (
     <>
@@ -25,7 +32,7 @@ const ChangeProgramButton = (props: Props) => {
         className="border-2 px-4 py-2">
         <div className="flex items-center gap-7">
           <span className="block ps-2 text-sm">{props.children}</span>
-          <ChevronUp className={cn(" duration-150", isOpen && 'rotate-180')} />
+          <ChevronUp className={cn("duration-150", isOpen && "rotate-180")} />
         </div>
       </Button>
       <Modal
@@ -55,7 +62,6 @@ const ChangeProgramButton = (props: Props) => {
         classNames={{
           base: "p-0 bg-transparent",
           closeButton: "hidden",
-
         }}
         isOpen={isOpen}
         onOpenChange={onOpenChange}>
@@ -70,8 +76,13 @@ const ChangeProgramButton = (props: Props) => {
                         shadow={"none"}
                         key={course.id}
                         isPressable
-                        as={Link}
-                        href={`/course/${course.id}`}
+                        onClick={() => {
+                          if (course_id == course.id) {
+                            onClose()
+                            return
+                          }
+                          setCourse(course.id)
+                        }}
                         radius="lg"
                         className="max-w-[390px] flex-grow border-none lg:w-1/2">
                         <Image
@@ -83,6 +94,47 @@ const ChangeProgramButton = (props: Props) => {
                       </Card>
                     )
                   })}
+                </div>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Modal
+        closeButton={null}
+        placement="center"
+        size="sm"
+        classNames={{
+          closeButton: "hidden",
+          base: "bg-[#0A090959] backdrop-blur-lg",
+        }}
+        isOpen={!!course}
+        onOpenChange={(value) => {
+          if (!value) setCourse("")
+        }}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody>
+                <div className="space-y-8 py-4">
+                  <p className="text-center text-lg font-medium text-foreground">
+                    {t("change-program-confirm-dialog")}
+                  </p>
+                  <div className="flex items-center justify-center gap-5">
+                    <MyButton size="sm" as={Link} href={`/course/${course}`}>
+                      {t("change-program-dialog-confirm")}
+                    </MyButton>
+                    <MyButton
+                      onClick={() => {
+                        setCourse("")
+                      }}
+                      size="sm"
+                      color="primary"
+                      className="text-primary"
+                      variant="bordered">
+                      {t("change-program-dialog-cancel")}
+                    </MyButton>
+                  </div>
                 </div>
               </ModalBody>
             </>
