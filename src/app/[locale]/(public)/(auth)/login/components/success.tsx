68d318f8928b8e6a400efc69cc2a89395467ec69
X_V.neer <xv.neer@gmail.com>
2024-12-02 14:40:48 +0200

    feat: fix redirect error

--- src/app/[locale]/(public)/(auth)/login/components/success.tsx
+++ src/app/[locale]/(public)/(auth)/login/components/success.tsx
@@ -4,15 +4,15 @@ import { successLogin } from "@/assets"
 import { Image } from "@nextui-org/image"
 import { useTranslations } from "next-intl"
 
-import { Link } from "@/lib/i18n/navigation"
 import Button from "@/components/ui/button"
-import { useSearchParams } from "next/navigation"
+import { useParams, useSearchParams } from "next/navigation"
 
 type Props = {}
 
 const Success = (props: Props) => {
   const searchParams = useSearchParams()
   const callbackUrl = searchParams.get("callbackUrl")
+  const params = useParams() as { locale: string }
   const t = useTranslations("login.success")
 
   // handle change mobile
@@ -24,7 +24,7 @@ const Success = (props: Props) => {
         <h2 className="text-center text-xl font-semibold">{t("title")}</h2>
       </div>
 
-      <Button as={Link} href={callbackUrl || `/start`}>
+      <Button as={"a"} href={`/${params.locale}${callbackUrl || `/start`}`}>
         {t("button")}
       </Button>
     </div>
