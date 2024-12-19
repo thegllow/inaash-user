1c17725f85f46cada039f1995451303ea5e60230
X_V.neer <xv.neer@gmail.com>
2024-12-19 08:54:25 +0200

    fix certificate

--- src/app/[locale]/(public)/certificate/[course_id]/view/page.tsx
+++ src/app/[locale]/(public)/certificate/[course_id]/view/page.tsx
@@ -9,7 +9,7 @@ const Page = async ({ params }: { params: { course_id: string } }) => {
     <Card
       shadow={"none"}
       className="mx-auto mb-20 flex h-full w-full max-w-3xl shrink-0 items-center justify-center gap-4 rounded-xl bg-[#0A090959] p-1 ~/md:~py-8/10">
-      <Certificate certificate_qr_code={video.certificate_qr_code!} />
+      <Certificate certificate_qr_code={video.certificate_number!} />
     </Card>
   )
 }
