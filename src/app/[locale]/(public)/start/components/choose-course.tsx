9e1f52b5aa303be87c930ea6a8b81443156dece4
X_V.neer <xv.neer@gmail.com>
2025-01-19 00:25:10 +0200

    show course details on hover

--- src/app/[locale]/(public)/start/components/choose-course.tsx
+++ src/app/[locale]/(public)/start/components/choose-course.tsx
@@ -32,48 +32,65 @@ const ChooseCourse = (props: Props) => {
   return (
     <div className={cn("flex w-full flex-col items-center justify-center gap-4 md:flex-row")}>
       {props.videos.map((course) => {
-        if (course.id == selectedCourse)
-          return (
-            <Card shadow={"none"} key={course.id} radius="lg" className="max-w-[390px] border-none lg:w-1/2">
-              {/* <Image alt="first-course" className="object-cover" src={course.image} removeWrapper /> */}
-              <CardHeader className="absolute inset-0 z-10 flex-col !items-start gap-3 bg-[#272525E5] p-5">
-                <h4 className="text-xl font-semibold text-foreground">{course.title}</h4>
-                <p className="text-sm text-foreground">{course.description}</p>
-                <Chip className="bg-[#27252570]" startContent={<Timer size={18} />} radius="sm">
-                  {course.length}
-                </Chip>
-                <Button
-                  as={Link}
-                  href={`/course/${course.id}`}
-                  className="mt-auto"
-                  startContent={<PlayCircle />}>
-                  {t("start-course-button")}
-                </Button>
-              </CardHeader>
-              <Image
-                removeWrapper
-                className="z-0 h-full w-full object-cover"
-                src={course.logo}
-                alt={course.title}
-              />
-            </Card>
-          )
+        // if (course.id == selectedCourse)
+        //   return (
+        //     <Card
+        //       onMouseLeave={() => {
+        //         setSelectedCourse(null)
+        //       }}
+        //       shadow={"none"}
+        //       key={course.id}
+        //       radius="lg"
+        //       className="max-w-[390px] border-none lg:w-1/2">
+        //       {/* <Image alt="first-course" className="object-cover" src={course.image} removeWrapper /> */}
+        //       <CardHeader className="absolute inset-0 z-10 flex-col !items-start gap-3 bg-[#272525E5] p-5">
+        //         <h4 className="text-xl font-semibold text-foreground">{course.title}</h4>
+        //         <p className="text-sm text-foreground">{course.description}</p>
+        //         <Chip className="bg-[#27252570]" startContent={<Timer size={18} />} radius="sm">
+        //           {course.length}
+        //         </Chip>
+        //         <Button
+        //           as={Link}
+        //           href={`/course/${course.id}`}
+        //           className="mt-auto"
+        //           startContent={<PlayCircle />}>
+        //           {t("start-course-button")}
+        //         </Button>
+        //       </CardHeader>
+        //       <Image
+        //         removeWrapper
+        //         className="z-0 h-full w-full object-cover"
+        //         src={course.logo}
+        //         alt={course.title}
+        //       />
+        //     </Card>
+        //   )
 
         return (
           <Card
             shadow={"none"}
             key={course.id}
-            isPressable
-            onClick={() => {
-              setSelectedCourse(course.id)
-            }}
             radius="lg"
-            className="max-w-[390px] flex-grow border-none lg:w-1/2">
+            className="group max-w-[390px] flex-grow border-none lg:w-1/2">
+            <CardHeader className="absolute inset-0 z-10 flex-col !items-start gap-3 bg-[#272525E5] p-5 opacity-0 duration-300 group-hover:opacity-100">
+              <h4 className="text-xl font-semibold text-foreground">{course.title}</h4>
+              <p className="text-sm text-foreground">{course.description}</p>
+              <Chip className="bg-[#27252570]" startContent={<Timer size={18} />} radius="sm">
+                {course.length}
+              </Chip>
+              <Button
+                as={Link}
+                href={`/course/${course.id}`}
+                className="mt-auto"
+                startContent={<PlayCircle />}>
+                {t("start-course-button")}
+              </Button>
+            </CardHeader>
             <Image
-              alt="first-course"
-              className="h-full w-full object-cover"
-              src={course.logo}
               removeWrapper
+              className="z-0 h-full w-full object-cover"
+              src={course.logo}
+              alt={course.title}
             />
           </Card>
         )
