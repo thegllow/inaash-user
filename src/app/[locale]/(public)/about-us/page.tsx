be935f44a59fab5acdaaa37428fdc66f090991c3
X_V.neer <xv.neer@gmail.com>
2025-01-22 16:20:00 +0200

    fix icons

--- src/app/[locale]/(public)/about-us/page.tsx
+++ src/app/[locale]/(public)/about-us/page.tsx
@@ -110,7 +110,7 @@ export default async function Page({ params }: { params: Promise<{ locale: (type
                     height="76"
                     filterUnits="userSpaceOnUse"
                     colorInterpolationFilters="sRGB">
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
                     <feColorMatrix
                       in="SourceAlpha"
                       type="matrix"
@@ -140,7 +140,7 @@ export default async function Page({ params }: { params: Promise<{ locale: (type
                     height="76"
                     filterUnits="userSpaceOnUse"
                     colorInterpolationFilters="sRGB">
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
                     <feColorMatrix
                       in="SourceAlpha"
                       type="matrix"
@@ -225,7 +225,7 @@ export default async function Page({ params }: { params: Promise<{ locale: (type
                     height="76"
                     filterUnits="userSpaceOnUse"
                     colorInterpolationFilters="sRGB">
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
                     <feColorMatrix
                       in="SourceAlpha"
                       type="matrix"
@@ -255,7 +255,7 @@ export default async function Page({ params }: { params: Promise<{ locale: (type
                     height="76"
                     filterUnits="userSpaceOnUse"
                     colorInterpolationFilters="sRGB">
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
                     <feColorMatrix
                       in="SourceAlpha"
                       type="matrix"
