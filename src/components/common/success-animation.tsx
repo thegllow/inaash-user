4526d8f76160f1dd06baea9dcdb6131e85352ccb
X_V.neer <xv.neer@gmail.com>
2025-01-23 13:38:48 +0200

    feat: add success animation

--- src/components/common/success-animation.tsx
+++ src/components/common/success-animation.tsx
@@ -3,7 +3,7 @@ import successAnimation from "@/assets/success-animation.json"
 import Lottie from "lottie-react"
 
 const SuccessAnimation = () => {
-  return <Lottie animationData={successAnimation} />
+  return <Lottie className="mx-auto w-52" loop={false} animationData={successAnimation} />
 }
 
 export default SuccessAnimation
