be12ce21dcae5eb6d649e2d04a76be8c43be0fd3
X_V.neer <xv.neer@gmail.com>
2025-01-26 08:41:15 +0200

    fix coupon

--- src/app/[locale]/(public)/payment/[course_id]/components/coupon.tsx
+++ src/app/[locale]/(public)/payment/[course_id]/components/coupon.tsx
@@ -1,45 +1,45 @@
-"use client";
+"use client"
 
-import { Input } from "@nextui-org/input";
-import { useTranslations } from "next-intl";
-import { parseAsString, useQueryState } from "nuqs";
+import { Input } from "@nextui-org/input"
+import { useTranslations } from "next-intl"
+import { parseAsString, useQueryState } from "nuqs"
 
-import Button from "@/components/ui/button";
-import { useState } from "react";
-import { Video } from "@/types/public-videos-response";
+import Button from "@/components/ui/button"
+import { useState } from "react"
+import { Video } from "@/types/public-videos-response"
 
-type Props = Video;
+type Props = Video
 
 const Coupon = (props: Props) => {
-  const t = useTranslations("payment.coupon");
-  const [c, setC] = useState("");
-  const [coupon, setCoupon] = useQueryState("coupon", parseAsString.withDefault(""));
+  const t = useTranslations("payment.coupon")
+  const [c, setC] = useState("")
+  const [coupon, setCoupon] = useQueryState("coupon", parseAsString.withDefault(""))
 
   const handleApplyCoupon = () => {
-    setCoupon(c, { shallow: false });
-  };
+    setCoupon(c, { shallow: false })
+  }
 
   // Check for error condition
-  const isInvalidCoupon = (props.coupon && props.price === props.final_price && Number(props.price) >0);
-  
-  return (
-      <div className="flex gap-4">
-        <Input
-          value={c}
-          onChange={(e) => {
-            const value = e.target.value.toUpperCase().trim();
-            setC(value);
-            if (!value) setCoupon("", { shallow: false });
-          }}
-          placeholder={t("input-placeholder")}
-          isInvalid={!!isInvalidCoupon}
-          errorMessage={t("errors.invalidCoupon")}
-        />
-        <Button onClick={handleApplyCoupon} size="md" fullWidth={false}>
-          {t("coupon-button")}
-        </Button>
-      </div>
-  );
-};
+  const isInvalidCoupon = props.coupon && props.price === props.final_price && Number(props.price) > 0
 
-export default Coupon;
+  return (
+    <div className="flex gap-4">
+      <Input
+        value={c}
+        onChange={(e) => {
+          const value = e.target.value.toUpperCase().trim()
+          setC(value)
+          if (!value) setCoupon("", { shallow: false })
+        }}
+        placeholder={t("input-placeholder")}
+        isInvalid={!!isInvalidCoupon}
+        errorMessage={t("errors.invalidCoupon")}
+      />
+      <Button isDisabled={!c} onClick={handleApplyCoupon} size="md" fullWidth={false}>
+        {t("coupon-button")}
+      </Button>
+    </div>
+  )
+}
+
+export default Coupon
