f940d31f1df00e0a2c2f839be2d793de2659b471
Emadeldeen Soliman <emade09@gmail.com>
2025-02-04 10:09:19 +0200

    f

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,75 +1,70 @@
 import { Fira_Code as FontMono, Kufam as Urdu } from "next/font/google"
 import localFont from "next/font/local"
 
-export const Kufam = Kufam({
-  subsets: ['arabic', 'latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-  preload: true,
-});
+ 
 
-export const fontSans = localFont({
-  src: [
-    {
-      path: "../assets/fonts/IBMPlexSansArabic-Light.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/IBMPlexSansArabic-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-
-    {
-      path: "../assets/fonts/IBMPlexSansArabic-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/IBMPlexSansArabic-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/IBMPlexSansArabic-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-sans",
-})
-// export const urdu = localFont({
+// export const fontSans = localFont({
 //   src: [
 //     {
-//       path: "../assets/fonts/NotoNastaliqUrdu-Regular.ttf",
+//       path: "../assets/fonts/IBMPlexSansArabic-Light.ttf",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "../assets/fonts/IBMPlexSansArabic-Regular.ttf",
 //       weight: "400",
 //       style: "normal",
 //     },
 
 //     {
-//       path: "../assets/fonts/NotoNastaliqUrdu-Medium.ttf",
+//       path: "../assets/fonts/IBMPlexSansArabic-Medium.ttf",
 //       weight: "500",
 //       style: "normal",
 //     },
 //     {
-//       path: "../assets/fonts/NotoNastaliqUrdu-SemiBold.ttf",
+//       path: "../assets/fonts/IBMPlexSansArabic-SemiBold.ttf",
 //       weight: "600",
 //       style: "normal",
 //     },
 //     {
-//       path: "../assets/fonts/NotoNastaliqUrdu-Bold.ttf",
+//       path: "../assets/fonts/IBMPlexSansArabic-Bold.ttf",
 //       weight: "700",
 //       style: "normal",
 //     },
 //   ],
 //   variable: "--font-sans",
 // })
-export const urdu = Urdu({
-  weight:['400'],
+export const urdu = localFont({
+  src: [
+    {
+      path: "../assets/fonts/NotoNastaliqUrdu-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
 
+    {
+      path: "../assets/fonts/NotoNastaliqUrdu-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/NotoNastaliqUrdu-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/NotoNastaliqUrdu-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
   variable: "--font-sans",
 })
+// export const urdu = Urdu({
+//   weight:['400'],
+
+//   variable: "--font-sans",
+// })
 
 export const fontMono = FontMono({
   subsets: ["latin"],
