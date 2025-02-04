e6f81c50ed72151f31a78940ded4ca26d14304b4
X_V.neer <xv.neer@gmail.com>
2025-02-04 12:39:23 +0200

    fix claming a certificate

--- src/app/[locale]/(public)/certificate/[course_id]/claim/components/render.tsx
+++ src/app/[locale]/(public)/certificate/[course_id]/claim/components/render.tsx
@@ -4,12 +4,20 @@ import Rating from "./rating"
 import UserInfo from "./user-info"
 import { useVideo } from "../../context/video-context"
 import { useEffect } from "react"
+import { User } from "@/app/[locale]/(public)/profile/types"
+import { useRouter } from "@/lib/i18n/navigation"
+import { useParams } from "next/navigation"
 
-type Props = {}
+type Props = {
+  user: User
+}
 
-const Render = (props: Props) => {
+const Render = ({ user }: Props) => {
   const [step, setStep] = useQueryState("step", parseAsNumberLiteral([1, 2, 3]).withDefault(1))
   const video = useVideo()
+  const { course_id } = useParams() as { course_id: string }
+  const Router = useRouter()
+  if (user.full_name && step === 2) Router.push(`/certificate/${course_id}/view`)
   useEffect(() => {
     if (video.is_rated != "0") setStep(2)
   }, [video.is_rated, setStep])
--- src/app/[locale]/(public)/certificate/[course_id]/claim/page.tsx
+++ src/app/[locale]/(public)/certificate/[course_id]/claim/page.tsx
@@ -1,9 +1,21 @@
+import InaashApi from "@/services/inaash"
 import Render from "./components/render"
+import { UserResponse } from "@/app/[locale]/(public)/profile/types"
 
-type Props = {}
+import { auth } from "@/lib/auth/auth"
+import { redirect } from "@/lib/i18n/navigation"
 
-const page = (props: Props) => {
-  return <Render />
+const page = async (props: { params: Promise<{ locale: string }> }) => {
+  const params = await props.params
+
+  const { locale } = params
+
+  const session = await auth()
+  if (!session) redirect({ href: "/login", locale })
+  const response = await InaashApi.get<UserResponse>(`/user/users/${session?.user.id}`)
+  const user = response.data.data.item
+
+  return <Render user={user} />
 }
 
 export default page
