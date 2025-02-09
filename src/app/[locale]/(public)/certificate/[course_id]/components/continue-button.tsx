0e7d4cb4c94fcf799e2a3e3ad05304fa0d86ae1c
X_V.neer <xv.neer@gmail.com>
2025-02-09 09:43:32 +0200

    fix continue button in mobile

--- src/app/[locale]/(public)/certificate/[course_id]/components/continue-button.tsx
+++ src/app/[locale]/(public)/certificate/[course_id]/components/continue-button.tsx
@@ -2,15 +2,18 @@
 /* eslint-disable @next/next/no-img-element */
 import { Button } from "@nextui-org/button"
 import { useTranslations } from "next-intl"
-import { Link } from "@/lib/i18n/navigation"
+import { Link, useRouter } from "@/lib/i18n/navigation"
 
 
 
 const ContinueButton = ({course_id}: {course_id: string}) => {
   const t = useTranslations("certificate")
-
+  const Router = useRouter()
+  const navigate = ()=>{
+    Router.push(`/certificate/${course_id}/claim`)
+  }
   return (
-    <Button as={Link} href={`/certificate/${course_id}/claim`}>
+    <Button onClick={navigate} >
       {t("continue")}
     </Button>
 )
